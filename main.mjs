0e05308cf022361431812fde6690742bb5a05e9e
dansaadati <dsaadati@google.com>
2023-04-05 21:02:20 -0700

    fix: correct import for util

--- main.mjs
+++ main.mjs
@@ -15,7 +15,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-import {execaAndLog} from './util.mjs'
+import {execaAndLog} from './lib/util.mjs'
 import fs from 'fs-extra';
 import generateDevsite from './lib/generate-devsite.mjs';
 import {join} from 'path';
